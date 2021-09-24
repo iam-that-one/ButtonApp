a5d29ddbea5d61eedaa0c6dc063af9e4b1fa737e
iam-that-one <iam-that-one@users.noreply.github.com>
2021-09-24 12:28:59 +0300

    Start

--- app/src/main/java/com/example/buttonapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonapp/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.buttonapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+  class MainActivity : AppCompatActivity() {
     lateinit var plus : Button
     lateinit var minus : Button
-    lateinit var  lbl : TextView
+     lateinit  var  lbl : TextView
     var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,16 +19,35 @@ class MainActivity : AppCompatActivity() {
         plus = findViewById(R.id.plus)
         minus = findViewById(R.id.minus)
 
+        var getNum = lbl.text.toString()
         plus.setOnClickListener{(add())}
+
         minus.setOnClickListener{(minus())}
+
     }
 
     fun add(){
-        count = count + 1
-      lbl.setText(count.toString())
+
+        count += 1
+        lbl.text = count.toString()
+        // count.toString().also { lbl.text = it }
+        if (count > 0){
+            lbl.setTextColor(Color.parseColor("#00FF00"))
+        }
+        if (count == 0){
+            lbl.setTextColor(Color.parseColor("#000000"))
+        }
     }
-    fun minus(){
-        count = count - 1
-        lbl.setText(count.toString())
+    fun minus() {
+        count -= 1
+        lbl.text = count.toString()
+
+        if (count < 0){
+            lbl.setTextColor(Color.parseColor("#FF0000"))
+        }
+        if (count == 0){
+            lbl.setTextColor(Color.parseColor("#000000"))
+        }
     }
+
 }
